ab98b52cf798328488afc7306a52aef1659d3e63
Kelin Field <kfield111@hotmail.com>
2020-08-25 22:19:25 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,9 +1,9 @@
 class Project
-    attr_reader :name
+    attr_reader :title
     attr_accessor :backers
 
-  def initialize (name, backers = [])
-    @name = name
+  def initialize (title, backers = [])
+    @title = title
     @backers = backers
   end
 
